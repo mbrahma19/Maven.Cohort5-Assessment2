a73b77de863525aad89f1b0784288f4057a0362b
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-04 12:09:41 -0500

    Completed person class and tests

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
@@ -5,40 +5,88 @@ package rocks.zipcode.assessment2.objectorientation;
  * @ATTENTION_TO_STUDENTS - Ensure that you have completed the `Address` class before attempting this class
  */
 public class Person {
+
+    private Long id;
+    private String name;
+    private Address address;
+
     /**
      * @param id - id of person
      * @param name - name of person
      * @param address - address of person
      */
     public Person(Long id, String name, Address address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
     }
 
     public Person() {
+        this(Long.MIN_VALUE, "", new Address());
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Address getAddress() {
-        return null;
+        return address;
     }
 
     public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Person{id=%d, name='%s', address=%s}",id,name,address.toString());
     }
 
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+        if(!(o instanceof Person)){
+            return false;
+        }else {
+            Person personToCompare = (Person) o;
+            return idEquals(personToCompare.getId()) &&
+                    nameEquals(personToCompare.getName()) &&
+                    addressEquals(personToCompare.getAddress());
+        }
+    }
+
+    public boolean idEquals(Long id){
+        if(this.id == null){
+            return id == null;
+        }else{
+            return id.equals(this.id);
+        }
+    }
+
+    public boolean nameEquals(String name){
+        if(this.name == null){
+            return name == null;
+        }else{
+         return name.equals(this.name);
+        }
+    }
+
+    public boolean addressEquals(Address address){
+        if(this.address == null){
+            return address == null;
+        }else{
+            return address.equals(this.address);
+        }
     }
 }
