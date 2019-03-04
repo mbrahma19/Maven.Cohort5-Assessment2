fe5b75709eb813da625811c178f2568a31ff3ed1
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-04 11:22:54 -0500

    Completed generics shelter

--- src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
+++ src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
@@ -1,5 +1,9 @@
 package rocks.zipcode.assessment2.generics;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 /**
  * Shelter is a generic class that holds Objects that extends `Ageable`.
@@ -8,33 +12,53 @@ package rocks.zipcode.assessment2.generics;
  * Shelter<Person> farmHouse = new Shelter<Person>();
  * Shelter<Dog> dogHouse = new Shelter<Dog>();
  */
-public class Shelter<_> {
+public class Shelter<Ageable> implements Iterable{
+
+    private ArrayList<Ageable> list;
+
     public Shelter() {
-        throw new NullPointerException();
+        list = new ArrayList<>();
     }
 
     /**
      * @return the number of item in the shelter
      */
     public int size() {
-        return -1;
+        return list.size();
     }
 
-    public void add(Object object) {
+    public void add(Ageable object) {
+        list.add(object);
     }
 
-    public Boolean contains(Object object) {
-        return null;
+    public Boolean contains(Ageable object) {
+        return list.contains(object);
     }
 
-    public void remove(Object object) {
+    public void remove(Ageable object) {
+        list.remove(object);
     }
 
     public Object get(Integer index) {
+        return list.get(index);
+    }
+
+    public Integer getIndexOf(Ageable ageable) {
+        return list.indexOf(ageable);
+    }
+
+    @Override
+    public Iterator iterator() {
         return null;
     }
 
-    public Integer getIndexOf(Object ageable) {
-        return 0;
+    @Override
+    public void forEach(Consumer action) {
+
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return null;
     }
 }
